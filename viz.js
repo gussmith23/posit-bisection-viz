726bd3591fc680e3d07c9b205f55bc9b5de0bbfd
Gus Smith <gussmith@cs.washington.edu>
2019-05-01 15:59:57 -0700

    Add margin; separate out cleanly

--- viz.js
+++ viz.js
@@ -3,12 +3,13 @@
  * Draw the projective reals line on an SVG.
  * @param svgSelection - the d3 selection of the SVG element.
  */
-function drawProjectiveRealsLine(svgSelection) {
-    var width = svgSelection.attr('width');
-    var height = svgSelection.attr('height');
+function drawProjectiveRealsLine(svgSelection, width, height) {
+    // An assumption I'm making right now.
+    console.assert(width === height);
+
     var origin = {x : width/2, y : height};
     var dest = {x : width/2, y : 0};
-    var radius = width/2;
+    var radius = (width)/2;
     container.append('path')
         .attr('d',
               'M' + origin.x + ' ' + origin.y + ' '
