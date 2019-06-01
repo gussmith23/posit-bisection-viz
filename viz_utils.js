6f47c31f5ba03173ad78c47bafe9920a63b9efd3
Neil Ryan <neilryan@cs.washington.edu>
2019-05-31 23:04:31 -0700

    Log scale (#53)
    * Working switch between linear scale and no scale
    
    * Working log scale, labels are smushed but that'll get fixed later

--- viz_utils.js
+++ viz_utils.js
@@ -8,6 +8,11 @@ const label_format = {
     BITSTRING: 'bitstring'
 };
 
+const scale_format = {
+    LINEAR: 'linear',
+    LOG: 'log'
+};
+
 const psign= {
     POSITIVE: 0,
     NEGATIVE: 1
@@ -56,10 +61,32 @@ function calculateYCenter(n) {
  * @brief Calculate the angle spacing between points on the posit circle 
  * @param n is the current n used for decoding posits
  * @returns The angle difference to use
+ * TODO return a list of angles instead, intervals based scaleFormat
  */
-function calculateDTheta(n) {
-    // TODO(gus) magic numbers
-    return 178/(1 << (n-1));
+function calculateDTheta(n, posits) {
+    var total_angle = 170; // How many degrees we have to play with
+    var dthetas = [];
+    var angle;
+    var num_posits = 1 << n;
+    if (scaleFormat == scale_format.LINEAR) {
+        var last_angle = 0;
+        for (i = 0; i < posits.length; i++) {
+            angle = total_angle/(num_posits/2) + last_angle;
+            dthetas.push(angle);
+            last_angle = angle;
+        }
+    } else {
+        var max_val = Math.abs(posits[posits.length - 1].value);
+        var min_val = Math.abs(posits[0].value);
+        var val_range = max_val - min_val;
+        for (i = 0; i < posits.length; i++) {
+            var val = Math.abs(posits[i].value);
+            var percent_of_range = (val - min_val) / val_range;
+            angle = total_angle * percent_of_range;
+            dthetas.push(angle);
+        }
+    }
+    return dthetas;
 }
 
 /**
